e7a4520cd3bf045e72038e75676974a1998520a3
Taynan Siqueira <taynacid123@gmail.com>
2019-01-10 22:12:52 -0200

    add store creator along with initial reducers and middleware

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+import authedUser from "../reducers/authedUser";
+import users from "../reducers/users";
+import tweets from "../reducers/tweets";
+
+export default combineReducers({
+  authedUser,
+  users,
+  tweets
+});
